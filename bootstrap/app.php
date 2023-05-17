c8e4c30e760da8bfdd3540ea3003e0db6a0a834d
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-05-18 01:00:12 +0300

    setup autoloading using composer and create some directories like public, bootstrap

---
+++ bootstrap/app.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Main App booting point of our application
+ */
+session_start();
+require_once __DIR__.'/../vendor/autoload.php';
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__.'/../bootstrap/app.php';
+use App\Config\Config;
+
+var_dump(new Config);
